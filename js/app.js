5274b977d834389105b5525ebe5ca3c5347e1e6f
saeed <120129486+saeedsayed@users.noreply.github.com>
2023-11-23 20:23:28 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,35 +1,84 @@
-let btnOptionOne= document.querySelector('.option-one button')
-let liNumWin= document.querySelector('.option-one ul')
-let btnOptionTow= document.querySelector('.option-tow button')
-let gameMode= document.querySelector('.option-tow ul')
-let playArea= document.querySelector('.body')
-let header= document.querySelector('header')
-btnOptionOne.addEventListener('click',_=>{
-    liNumWin.classList.toggle('show')
+
+let btnOptionTow = document.querySelector('.option-tow button')
+let gameMode = document.querySelector('.option-tow ul')
+let playArea = document.querySelector('.body')
+let header = document.querySelector('header')
+let openFormBtn = document.querySelector('#openForm')
+let closeFormBtn = document.querySelector('#closeForm')
+let addPlayersForm = document.querySelector('.add-player__form')
+let playersInputs = document.querySelectorAll('.add-player__form--inputs input')
+let addPlayerBtn = document.querySelector('#addPlayerBtn')
+let addInputToForm = document.querySelector('#addInputToForm')
+let btnPlay = document.querySelector('.btn-play')
+let winElement = document.querySelector('.winner')
+let losElement = document.querySelector('.loser')
+let winNameElement = document.querySelector('.win-name')
+let loseNameElement = document.querySelector('.lose-name')
+
+let players = []
+if (localStorage.getItem('playersName')) {
+    players = JSON.parse(localStorage.getItem('playersName'))
+}
+let winner = ''
+let loser = ''
+
+openFormBtn.addEventListener('click', _ => {
+    addPlayersForm.classList.add('show')
 })
-btnOptionTow.addEventListener('click',_=>{
-    gameMode.classList.toggle('show')
+closeFormBtn.addEventListener('click', _ => {
+    addPlayersForm.classList.remove('show')
 })
 
-
-
-playArea.addEventListener('touchstart', e => {
-    let top = e.touches[0].pageY
-    let left = e.touches[0].pageX
-    gameMode.classList.remove('show')
-    liNumWin.classList.remove('show')
-    header.style.top = '-100%'
-    playArea.innerHTML = `<div class="player" style='top:${top}px;left:${left}px'></div>`
+addInputToForm.addEventListener('click', _ => {
+    let div = document.createElement('div')
+    div.innerHTML = '<input type="text"> <button onclick=deleteInput(this)>حذف</button>'
+    document.querySelector('.add-player__form--inputs').append(div)
+    playersInputs = document.querySelectorAll('.add-player__form--inputs input')
 })
 
-playArea.addEventListener('touchmove', e => {
-    let top = e.touches[0].pageY
-    let left = e.touches[0].pageX
-    let player = document.querySelector('.player')
-    player ? player.style = `top:${top}px;left:${left}px` : null;
+btnPlay.addEventListener('click', _ => {
+    if (players.length) {
+        losElement.classList.add('show')
+        winElement.classList.add('show')
+        randomChoose()
+    } else {
+        addPlayersForm.classList.add('show')
+    }
 })
 
-playArea.addEventListener('touchend',_=>{
-    playArea.innerHTML=``
-    header.style.top='0'
-})
+addPlayerBtn.addEventListener('click', addPlayers)
+
+function deleteInput(e) {
+    e.parentElement.remove()
+}
+
+
+function addPlayers() {
+    players = []
+    playersInputs.forEach(input => {
+        input.value !== '' && players.push(input.value)
+    })
+    addPlayersForm.classList.remove('show')
+    localStorage.setItem('playersName', JSON.stringify(players))
+    console.log(players);
+}
+
+function randomChoose() {
+    let randomWin = Math.floor(Math.random() * players.length)
+    let randomLos = Math.floor(Math.random() * players.length)
+    let interval = setInterval(() => {
+        randomWin = Math.floor(Math.random() * players.length)
+        randomLos = Math.floor(Math.random() * players.length)
+        winNameElement.innerHTML = players[randomWin]
+        loseNameElement.innerHTML = players[randomLos]
+    }, 30);
+    setTimeout(() => {
+        clearInterval(interval)
+    }, 2000);
+    if (randomLos != randomWin) {
+        winNameElement.innerHTML = players[randomWin]
+        loseNameElement.innerHTML = players[randomLos]
+    } else {
+        randomChoose()
+    }
+}
